c83d3c2a718cf7f8f1ac1d82947ac81f70eb2226
Spicy-Rabbit-Head [zhao'zi'kui] <1192469846@qq.com>
2023-09-10 20:38:36 +0800

    更新

--- management/TableDesign.sql
+++ management/TableDesign.sql
@@ -4,8 +4,21 @@
  */
 -- 创建用户管理架构
 CREATE SCHEMA IF NOT EXISTS user_management;
+
 -- 创建用户表
-CREATE TABLE
+CREATE TABLE IF NOT EXISTS user_management.user_data
+(
+    -- 用户主键
+    id       INTEGER PRIMARY KEY,
+    -- 用户名
+    username VARCHAR(45) NOT NULL,
+    -- 密码
+    password VARCHAR(45) NOT NULL,
+    -- 邮箱
+    email    VARCHAR(45) NOT NULL,
+    -- 通用唯一识别码
+    uuid     VARCHAR(45) NOT NULL
+);
 
 
 
